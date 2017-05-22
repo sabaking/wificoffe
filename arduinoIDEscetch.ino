f7708c342e720bc67e36b747ad7cd71d0171997a
Denis <mashkov@gmail.com>
2017-05-23 00:34:30 +0300

    In progress

--- arduinoIDEscetch.ino
+++ arduinoIDEscetch.ino
@@ -1,8 +1,7 @@
-/* Change your ssid & password */
 #include <ESP8266WiFi.h>
 
-const char* ssid = "wifilogin";
-const char* password = "wifipassword";
+const char* ssid = "zhdanya";
+const char* password = "Qwe12345";
 
 // start web server
 WiFiServer server(80);
@@ -10,13 +9,15 @@ WiFiServer server(80);
 void setup() {
   delay(10);
 
+  // prepare GPIO0
+  pinMode(0, OUTPUT);
+  digitalWrite(0, 0); // D3 on NodeMCU
+  
   // prepare GPIO2
   pinMode(2, OUTPUT); //  D4 on NodeMCU
   digitalWrite(2, 0);
   
-  // prepare GPIO0
-  pinMode(0, OUTPUT);
-  digitalWrite(0, 0); // D3 on NodeMCU
+
   
   // Connect to WiFi 
   WiFi.begin(ssid, password);
@@ -49,27 +50,30 @@ void loop() {
   
   // Read the first line of the request
   String req = client.readStringUntil('\r');
-  Serial.println(req);
   client.flush();
-  
   // Match the request
-  int val_makecoffe;
-  int val_stop;
-  if (req.indexOf("/makecoffee") != -1)
-    val_makecoffe = 1;
-  else if (req.indexOf("/stop") != -1)
-    val_stop = 1;
+  if (req.indexOf("/makecoffee") != -1){
+      // Set GPIO2 & GPIO0 according to the request 
+      digitalWrite(0, 1); // push power button on coffe machine
+      delay(5000);
+      digitalWrite(0, 0); // unpush power button on coffe machine 
+      delay(10000); // some delay beetween push the buttons
+      digitalWrite(2, 1); // push start button on cm
+      delay(5000);
+      digitalWrite(2, 0); // unpush start button on cm
+  }
+  else if (req.indexOf("/stop") != -1){
+      // set alert stop , poweroof & push stop button on coffeemachine - 
+      digitalWrite(2, 0);
+      digitalWrite(0, 0);
+      delay(500);
+      digitalWrite(2, 0); 
+  }
   else {
     Serial.println("invalid request");
     client.stop();
     return;
   }
-  // set alert stop 
-  digitalWrite(2, val_stop); 
-  // Set GPIO2 & GPIO0 according to the request 
-  digitalWrite(0, val_makecoffe); // push power button on coffe machine
-  delay(15); // some delay beetween push the buttons
-  digitalWrite(2, val_makecoffe); // push start button on cm
   
   client.flush();
 
